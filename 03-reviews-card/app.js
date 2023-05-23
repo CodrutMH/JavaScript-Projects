67d38dd2a025f49c86fa1c7f96409efe5fcf67e0
CodrutMH <codrutmihalca@gmail.com>
2023-05-23 20:45:38 +0300

    succesfully review project

--- 03-reviews-card/app.js
+++ 03-reviews-card/app.js
@@ -1,55 +1,86 @@
-/*
-=============== 
-Fonts
-===============
-*/
-@import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto:400,700&display=swap');
+const people = [
+  {
+    img: 'imgs/lanarhoades.jpg',
+    name: 'Lana Rhoades',
+    job: 'web developer',
+    info: 'I LOVE BEEING A WEB DEVELOPER, OMG',
+  },
+  {
+    img: 'imgs/jhonny.jpg',
+    name: 'Jhonny Sins',
+    job: 'TEAM LIDER',
+    info: 'TEAM LIDER, HEHEHEHEHEHEEHEHE',
+  },
+  {
+    img: 'imgs/elsajean.jpg',
+    name: 'Elsa Jean',
+    job: 'Angular Developer',
+    info: 'I LOVE BEEING A Angular DEVELOPER, OMG',
+  },
+  {
+    img: 'imgs/miakalifa.jpg',
+    name: 'Mia Khalifa',
+    job: 'SEO',
+    info: 'I LOVE BEEING A SEO, OMG',
+  },
+];
 
-/*
-=============== 
-Variables
-===============
-*/
+// select items
 
-:root {
-  /* dark shades of primary color*/
-  --clr-primary-1: hsl(205, 86%, 17%);
-  --clr-primary-2: hsl(205, 77%, 27%);
-  --clr-primary-3: hsl(205, 72%, 37%);
-  --clr-primary-4: hsl(205, 63%, 48%);
-  /* primary/main color */
-  --clr-primary-5: hsl(205, 78%, 60%);
-  /* lighter shades of primary color */
-  --clr-primary-6: hsl(205, 89%, 70%);
-  --clr-primary-7: hsl(205, 90%, 76%);
-  --clr-primary-8: hsl(205, 86%, 81%);
-  --clr-primary-9: hsl(205, 90%, 88%);
-  --clr-primary-10: hsl(205, 100%, 96%);
-  /* darkest grey - used for headings */
-  --clr-grey-1: hsl(209, 61%, 16%);
-  --clr-grey-2: hsl(211, 39%, 23%);
-  --clr-grey-3: hsl(209, 34%, 30%);
-  --clr-grey-4: hsl(209, 28%, 39%);
-  /* grey used for paragraphs */
-  --clr-grey-5: hsl(210, 22%, 49%);
-  --clr-grey-6: hsl(209, 23%, 60%);
-  --clr-grey-7: hsl(211, 27%, 70%);
-  --clr-grey-8: hsl(210, 31%, 80%);
-  --clr-grey-9: hsl(212, 33%, 89%);
-  --clr-grey-10: hsl(210, 36%, 96%);
-  --clr-white: #fff;
-  --clr-red-dark: hsl(360, 67%, 44%);
-  --clr-red-light: hsl(360, 71%, 66%);
-  --clr-green-dark: hsl(125, 67%, 44%);
-  --clr-green-light: hsl(125, 71%, 66%);
-  --clr-black: #222;
-  --ff-primary: 'Roboto', sans-serif;
-  --ff-secondary: 'Open Sans', sans-serif;
-  --transition: all 0.3s linear;
-  --spacing: 0.1rem;
-  --radius: 0.25rem;
-  --light-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
-  --dark-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
-  --max-width: 1170px;
-  --fixed-width: 620px;
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.js-prev-btn');
+const nextBtn = document.querySelector('.js-next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// set strarting item
+
+let currentItem = 0;
+
+// load initial item
+
+window.addEventListener('DOMContentLoaded', function () {
+  showPerson(currentItem);
+});
+
+// show person on item
+
+function showPerson(person) {
+  const item = people[person];
+  console.log(item);
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.info;
 }
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > people.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = people.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+randomBtn.addEventListener('click', function () {
+  const randomPerson = Math.round(Math.random() * people.length - 1);
+  console.log(randomPerson);
+  currentItem = randomPerson;
+  if (randomPerson > people.length - 1) {
+    currentItem = 0;
+  } else if (randomPerson < 0) {
+    currentItem = people.length - 1;
+  }
+  showPerson(currentItem);
+  console.log(randomPerson);
+});
